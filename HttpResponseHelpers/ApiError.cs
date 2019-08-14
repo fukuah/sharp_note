6b721678416f4179e7763edfb568d4841a0348bb
fukuah <zetx44@yandex.ru>
2019-08-14 22:07:22 +0300

    Response helpers added: + ApiResponse + ApiError

---
+++ HttpResponseHelpers/ApiError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpNote.ApiResponseHelpers
+{
+    public class ApiError
+    {
+        public int _code { get; }
+        public string _message { get; }
+    }
+}
---
+++ HttpResponseHelpers/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpNote.ApiResponseHelpers
+{
+    public class ApiResponse { }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T body { get; }
+        public ApiError error { get; }
+
+        public ApiResponse(T bdata)
+        {
+            body = bdata;
+        }
+    }
+}
